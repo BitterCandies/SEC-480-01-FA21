9ef55c941c00bec90b62e8bfb700f3e2e332a311
BitterCandies <starblaze647@gmail.com>
2021-10-20 06:03:25 -0400

    480-util

--- 480-utils.ps1
+++ 480-utils.ps1
@@ -1,19 +1,25 @@
+# LEFT OFF WHERE THERE IS A PROBLEM WITH THE PROGRAM EXITING REGARDLESS OF WHAT IT'S DOING WITH CONNECTVCENTER FUNCTION
+
 $vser = "vcenter.tao.local"
 $snshname = "Safe"
 $vmhost = "192.168.3.20"
 $ds = "datastore2-super10"
-#Disconnect-VIServer -Server * -Force -ea SilentlyContinue
+Disconnect-VIServer -Server * -Y -ea SilentlyContinue
 
 Function connectvcenter {
     $tryvseragain = "y"
+
     while ($tryvseragain -notmatch '^[nN]$') {
         $vseranswer = Read-Host -Prompt "Enter the vcenter server you would like to connect to [default is $vser]"
+        
         if ($vseranswer) {
             Write-Host "Connecting to vcenter server $vseranswer..."
             $connpos = Connect-VIServer($vseranswer) -ErrorVariable err -ea SilentlyContinue
+            
             if ($err) {
                 $tryvseragain = Read-Host -Prompt "There was an issue using the vcenter server $vseranswer. Would you like to try again?
 [Y]es try again, or [N]o"
+                
                 if ($tryvseragain -notmatch '^[yY]$') {
                     Write-Host "Exiting..."
                     exit
@@ -21,12 +27,15 @@ Function connectvcenter {
             }
             $tryvseragain = "n"
             $moveon = 'y'
+
         } else {
             Write-Host "Using default vcenter server..."
             $connpos = Connect-VIServer($vser) -ErrorVariable err -ea SilentlyContinue
+            
             if ($err) {
                 $tryvseragain = Read-Host -Prompt "There was an issue using the vcenter server $vser. Would you like to try again?
 [Y]es try again, or [N]o"
+
                 if ($tryvseragain -notmatch '^[yY]$') {
                     Write-Host "Exiting... $connpos"
                     exit
